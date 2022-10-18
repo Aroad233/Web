27f134ca9e06a56a10bd13491eaa046359f42570
Aroad <41125391+Aroad233@users.noreply.github.com>
2022-10-18 14:38:47 +0800

    Delete yygh/yygh_parent directory

--- yygh/yygh_parent/service/service_hosp/src/main/java/com/tsuiraku/hospservice/mapper/ScheduleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.tsuiraku.hospservice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tsuiraku.modelservice.model.hosp.Schedule;
-
-public interface ScheduleMapper extends BaseMapper<Schedule> {
-}
--- yygh/yygh_parent/service/service_user/src/main/java/com/tsuiraku/userservice/mapper/UserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.tsuiraku.userservice.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tsuiraku.modelservice.model.user.UserInfo;
-
-public interface UserInfoMapper extends BaseMapper<UserInfo> {
-}
